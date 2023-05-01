56dfab9081c42ab4b91ce674e27ee0539ad5e5f7
Кристиян Кирилов <82154143+kristiyan8@users.noreply.github.com>
2023-05-01 21:19:32 +0300

    qeqe
    qeq

---
+++ WebApplication2 - main/WebApplication2/models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class Class1
+{
+	public Class1()
+	{
+		public int  ID { get; set; }
+	public string Name { get; set; }
+	
+
+}
+}
